56ce4dab09f0cd5fd9fdc0879e85072fb38255d6
Matt S <XxLMM13xXgaming@gmail.com>
2016-11-06 16:56:58 -0500

    Woah how i forget this???

--- lua/autorun/lmm_npcr_load.lua
+++ lua/autorun/lmm_npcr_load.lua
@@ -1,4 +1,7 @@
 if (SERVER) then
+	if not file.Exists("lmmnpcr_data", "DATA") then
+		file.CreateDir("lmmnpcr_data")
+	end
 	AddCSLuaFile('lmm_npcr_config.lua')
 	include('lmm_npcr_config.lua')
 end
